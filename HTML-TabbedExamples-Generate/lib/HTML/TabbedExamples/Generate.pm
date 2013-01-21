4be87ea4a254acdf5d804dfcd7bb7ebd18ae4ee8
Shlomi Fish <shlomif@shlomifish.org>
2013-01-21 23:37:01 +0200

    Got the tests to pass.

--- HTML-TabbedExamples-Generate/lib/HTML/TabbedExamples/Generate.pm
+++ HTML-TabbedExamples-Generate/lib/HTML/TabbedExamples/Generate.pm
@@ -3,5 +3,16 @@ package HTML::TabbedExamples::Generate;
 use strict;
 use warnings;
 
+our $VERSION = '0.0.1';
+
 1;
+
+=encoding utf-8
+
+=head1 NAME
+
+HTML::TabbedExamples::Generate - generate syntax-highlighted examples for
+codes with a markup compatible with jQueryUI's tab widgets.
+
+=cut
 
